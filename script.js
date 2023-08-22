ef0e709900ef7aa821aca54b1bb4f39d7641c95d
Aaron Yoder <ayoder243@gmail.com>
2023-08-22 10:44:03 -0500

    Add CSS to display grid
    Too many changes for 1 commit.
    Linked script.js and style.css in index.html
    Fixed createGrid(length) by adding grid-template-column and
    grid-template-rows.

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const GRID_WIDTH = 960;
+const GRID_LENGTH = 960;
+
 function createGrid(length=16) {
   let sketchpad = document.querySelector(".sketchpad");
+  sketchpad.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+  sketchpad.style.gridTemplateRows = `repeat(${length}, 1fr)`;
   for(let i = 0; i < length; i++) {
     for(let j = 0; j < length; j++) {
       let box = document.createElement('div');
+      box.classList.add("box");
+      box.setAttribute("grid-column", i + 1);
+      box.setAttribute("grid-row", j + 1);
       sketchpad.appendChild(box);
     }
   }
 }
+
+createGrid();
